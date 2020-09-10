db577802c13b929122f215cf4e2c4daad564400b
AviranMelamed <aviranmelamed@gmail.com>
2020-09-10 13:16:46 +0300

    add branch

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require(`express`)
 const app = express()
 
+app.use(express.static(path.join(__dirname, `dist`)))
+app.use(express.static(path.join(__dirname, `node_modules`)))
 
 const port = 3000
 app.listen(port, function(){
